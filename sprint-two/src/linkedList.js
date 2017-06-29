d88c52cd9929f586e7fb7ff10737f14ac0bd32f1
Hack Reactor Student <student@hackreactor.com>
2017-06-28 19:54:15 -0500

    completed linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,44 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+    } else {
+      function recursion(obj, nextVal) {
+        if (nextVal === null) {
+          obj.next = newNode;
+        } else if (nextVal) {
+          recursion(nextVal, nextVal.next);
+        }
+      }
+      recursion(list.head, list.head.next);
+    }
+    //don't need a recursion right here - use tail instead - think about it!
+    list.tail = newNode;
+    //1) create a node
+    //2) check if head === null - refer the list.head to the new node just created
+    //3) refer the list.tail to the new node
+    //4) if there's a previous node - link the previous's node's next to the current node
   };
 
   list.removeHead = function() {
+    var preVal = list.head.value;
+    list.head = list.head.next;
+    return preVal;
   };
 
   list.contains = function(target) {
+    function recursingAgain(obj) {
+      if (obj.value === target) {
+        return true;
+      } else if (obj.next === null) {
+        return false;
+      } else {
+        return recursingAgain(obj.next);
+      }
+    }
+    return recursingAgain(list.head);
   };
 
   return list;
