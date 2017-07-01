7e83ec82780585166a626af7546465d798be9f5c
Hack Reactor Student <student@hackreactor.com>
2017-07-01 11:24:56 -0500

    Finish all tests and finished advanced hashTable tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,36 +7,28 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var counter = 1;
-  this._storage.each(function(item) { 
-    if (item !== undefined) {
-        //return console.log(item, 'hi?')
-      for (var i = 0; i < item.length; i ++) {
-        counter++;
-      }
-    }
-  });
-  // console.log(counter, 'here check counter');
-  if (counter >= (this._limit * .75)) {
+  if (this.expandSize()) {
     this.reHash();
-  } else {
-    var bucket = this._storage.get(index) || [];
-    this._storage.set(index, bucket);
-    for (var i = 0; i < bucket.length; i++) {
-      if (bucket[i][0] === k) {
-        bucket[i][1] = v;
-        return;
-      }
-    }
-    bucket.push([k, v]);
-  //this.checkUsage();
+    index = getIndexBelowMaxForKey(k, this._limit);
   }
+  
+  var bucket = this._storage.get(index) || [];
+  this._storage.set(index, bucket);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      bucket[i][1] = v;
+      return;
+    }
+  }
+  bucket.push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
-  
+  if (bucket === undefined) {
+    return undefined;
+  }
   for (var i = 0; i < bucket.length; i++) {
     if (bucket[i][0] === k) {
       return bucket[i][1];
@@ -49,25 +41,78 @@ HashTable.prototype.retrieve = function(k) {
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
-  
   for (var i = 0; i < bucket.length; i++) {
     if (bucket[i][0] === k) {
       bucket.splice(i, 1);
     }
-  }  
+  }
+  //check right after one ite,m is removed - do we required to reduce size?
+  if (this.reduceSize()) {
+    this.reHash();
+  }
+};  
   
- //  HashTable.prototype.checkUsage = function() {
- //  //if > 75% (limited * .75) expand, if < 25%
- // // return true/false that help it to determine if its time to expand or not
- // //shoudl be invoked in insert 
- //  };
+HashTable.prototype.checkUsage = function() {
+  var counter = 0;
+  this._storage.each(function(item) { 
+    if (item !== undefined) {
+      for (var i = 0; i < item.length; i ++) {
+        counter++;
+      }
+    }
+  });
+//if > 75% (limited * .75) expand, if < 25%
+// return true/false that help it to determine if its time to expand or not
+//shoudl be invoked in insert 
 
-  HashTable.prototype.reHash = function() {
-    //should create a new limitedarray that takes in all values from old storage
-    //^ once its done
-  };
-
+  return counter;
 };
+
+HashTable.prototype.expandSize = function() {
+  if (this.checkUsage() + 1 >= this._limit * .75) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+HashTable.prototype.reduceSize = function() {
+  if (this.checkUsage() < this._limit * .25) {
+    return true;
+  } else {
+    return false;
+  }    
+};
+
+HashTable.prototype.reHash = function() {
+  //should create a new limitedarray that takes in all values from old storage
+  //^ once its done
+  //if expandsize is true, then we times the array limit by 2
+  if (this.expandSize()) {
+    this._limit *= 2;
+  } else if (this.reduceSize()) {
+    this._limit /= 2;
+  }
+  
+  //if reducesize is true, then we divide by 2
+  
+  //create a new limitedarray with the new array limit pass in
+  var newLimited = LimitedArray(this._limit);
+  var useThisForScopeLimit = this._limit;
+  this._storage.each(function(item) {
+    if (item !== undefined) {
+      for (var i = 0; i < item.length; i++) {
+        var newIndex = getIndexBelowMaxForKey(item[i][0], useThisForScopeLimit);
+        var bucket = newLimited.get(newIndex) || [];
+        newLimited.set(newIndex, bucket);
+        bucket.push([item[i][0], item[i][1]]);
+      }
+    }
+  });
+  
+  this._storage = newLimited;
+};
+
 
 
 
